f9a44e4dde140e57f4a2c8b26ac122fda199f93b
guilhermefront <guilhermesnd14@gmail.com>
2023-02-12 17:30:33 -0300

    feat: update to use analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solid(), tailwind()],
+  output: 'server',
+  adapter: vercel({
+    analytics: true,
+  }),
 });
